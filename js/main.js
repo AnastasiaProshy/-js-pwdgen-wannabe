b8bb494078639c0ccc0597b9fc3a45031f293ae1
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-06 10:46:44 +0200

    end: step-one

--- js/main.js
+++ js/main.js
@@ -2,19 +2,24 @@ const firstName = prompt('Please enter your name');
 const lastName = prompt('Please enter your surname');
 const color = prompt('Please enter your favorite color');
 
-//OUTPUT STRING******************
+// //OUTPUT STRING******************
 const output = `
 
-******Dates*******
+// ******Dates*******
 
-name: ${firstName}
-surname: ${lastName}
-color: ${color}
+// name: ${firstName}
+// surname: ${lastName}
+// color: ${color}
 
-******Generate Password******
-
-`;
-
+// ******Generate Password******
+// `;
 
 
 console.log(output);
+
+
+const pWord = ` Suggested password: ${firstName}${lastName}${color}34`;
+
+console.log(pWord);
+
+document.getElementById('PW').innerHTML = pWord;
