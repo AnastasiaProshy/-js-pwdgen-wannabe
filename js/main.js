b9d4794563c07d31d9c05cd2d162a41932905e15
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-06 10:01:53 +0200

    add: requests

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const firstName = prompt('Please enter your name');
+const lastName = prompt('Please enter your surname');
+const color = prompt('Please enter your favorite color');
+
+//OUTPUT STRING******************
+const output = `
+
+******Dates*******
+
+name: ${firstName}
+surname: ${lastName}
+color: ${color}
+
+******Generate Password******
+
+`;
+
+
+
+console.log(output);
