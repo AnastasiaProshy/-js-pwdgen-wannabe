d67b2d00c391a77acbc568822f5101f24971ae32
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-06 20:37:36 +0200

    add: operazione

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const firstName = prompt('Please enter your name');
 const lastName = prompt('Please enter your surname');
 const color = prompt('Please enter your favorite color');
+const BYear = prompt(`Write your b-day year`);
+const Bday = prompt(`Write down the day you were born`);
 
-// //OUTPUT STRING******************
+//OUTPUT STRING******************
 const output = `
 
 ******Dates*******
@@ -10,6 +12,8 @@ const output = `
 name: ${firstName}
 surname: ${lastName}
 color: ${color}
+Birth-year: ${BYear}
+Birthday: ${Bday}
 
 ******Generate Password******
 `;
@@ -17,9 +21,23 @@ color: ${color}
 
 console.log(output);
 
+const pWord = ` Suggested password: ${firstName}${lastName}${color}`;
 
-const pWord = ` Suggested password: ${firstName}${lastName}${color}34`;
+//DOM output
+// console.log(pWord);
+// document.getElementById('PW').innerHTML = pWord;
+//OPERATION
+// const pWordN = ``;
 
-console.log(pWord);
+const pwN = `${BYear} / ${Bday}`;
 
-document.getElementById('PW').innerHTML = pWord;
+console.log(pwN);
+
+// Concatenazione di pWord e pwN
+
+const pWordN = ${ pWord }+${ pwN };
+
+console.log(pWordN);
+
+document.getElementById('PW').innerHTML = pWordN;
+
